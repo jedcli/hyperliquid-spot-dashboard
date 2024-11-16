6065b756f38cb71b4d99f8513ac032af91df8b05
Evgenii <jedcli@deadletter.com>
2024-11-16 22:00:44 +0100

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hyperliquid-spot-dashboard/', // Use your repository name here
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
